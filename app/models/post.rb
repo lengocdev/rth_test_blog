4afe25144510d39511ddd961e5a23c03d06ef46c
levanngoc <ngoclv26@gmail.com>
2020-03-10 22:43:59 +0700

    validate post (title, content)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-
+  validates :title, presence: true, length: { minimum: 3, maximum: 40}
+  validates :content, presence: true, length: { minimum: 10, maximum: 400}
 end
