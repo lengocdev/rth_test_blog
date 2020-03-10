1b84b6b9121afff4df147d73fb88f03464fc6ec1
levanngoc <ngoclv26@gmail.com>
2020-03-10 23:16:57 +0700

    create post form browser

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post was successfully created'
+      redirect_to post_path(@post)
+    else
+      render 'new'
+      end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
