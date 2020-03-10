2541819e5c9559c29809729e8e60c1fb54b7d164
levanngoc <ngoclv26@gmail.com>
2020-03-11 01:55:55 +0700

    Signing Up Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX}
