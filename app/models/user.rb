197ae21993555f9e6b1a6294cea298d494ec8598
levanngoc <ngoclv26@gmail.com>
2020-03-11 01:47:26 +0700

    validate mail and fix layout index post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
   validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 25}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true,
-            length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX}
 end
