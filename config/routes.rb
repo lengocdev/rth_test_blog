a559bb7b309bf31ab39e58dedd38bf5e7d084232
levanngoc <ngoclv26@gmail.com>
2020-03-11 02:30:25 +0700

    singup user on view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
-  root 'pages#index'
-
+  get 'signup', to: 'users#new'
   resources :posts
+  resources :users, except: [:new]
+
+  root 'pages#index'
 end
