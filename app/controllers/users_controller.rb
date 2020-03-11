dd0eacc9a2864e7055ab03773e9b8c853bd085d3
levanngoc <ngoclv26@gmail.com>
2020-03-11 09:43:59 +0700

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,6 +14,16 @@ class UsersController < ApplicationController
       redirect_to posts_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to posts_path
+    else
+      render 'edit'
     end
   end
 
