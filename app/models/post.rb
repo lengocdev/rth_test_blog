1f6a6dfc271a5bd1e7ff1e78ac7b03a8bdf2363f
levanngoc <ngoclv26@gmail.com>
2020-03-11 09:34:35 +0700

    fix bug email not downcase and not save the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,4 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 40}
   validates :content, presence: true, length: { minimum: 10, maximum: 400}
   validates :user_id, presence: true
-  before_save { self.email = email.downcase }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
+  before_save { self.email = email.to_s.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX}
